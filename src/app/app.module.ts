5f4348cfeb228986dee19a1e82485d378121bb72
Jcacero <jcruz2013c@gmail.com>
2022-10-03 23:21:41 -0300

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+import { PagesModule } from './pages/pages.module';
 import { PrimengModule } from './primeng/primeng.module';
 
 @NgModule({
@@ -15,6 +16,7 @@ import { PrimengModule } from './primeng/primeng.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    PagesModule,
     PrimengModule,
     BrowserAnimationsModule
   ],
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -17,31 +17,17 @@ export class NavbarComponent implements OnInit {
           label:'Peliculas',
           items:[
               {
-                  label:'New',
+                  label:'Terror',
                   icon:'pi pi-fw pi-plus',
-                  items:[
-                  {
-                      label:'Bookmark',
-                      icon:'pi pi-fw pi-bookmark'
-                  },
-                  {
-                      label:'Video',
-                      icon:'pi pi-fw pi-video'
-                  },
-
-                  ]
               },
               {
-                  label:'Delete',
-                  icon:'pi pi-fw pi-trash'
-              },
-              {
-                  separator:true
-              },
-              {
-                  label:'Export',
-                  icon:'pi pi-fw pi-external-link'
-              }
+                label:'Comedia',
+                icon:'pi pi-fw pi-plus',
+            },
+            {
+                label:'Documentales',
+                icon:'pi pi-fw pi-plus',
+            }
           ]
       },
       {
@@ -71,67 +57,24 @@ export class NavbarComponent implements OnInit {
           icon:'pi pi-fw pi-user',
           items:[
               {
-                  label:'New',
+                  label:'Actores / Actrices',
                   icon:'pi pi-fw pi-user-plus',
 
               },
               {
-                  label:'Delete',
+                  label:'Directores',
                   icon:'pi pi-fw pi-user-minus',
 
               },
               {
-                  label:'Search',
+                  label:'Guionistas',
                   icon:'pi pi-fw pi-users',
-                  items:[
-                  {
-                      label:'Filter',
-                      icon:'pi pi-fw pi-filter',
-                      items:[
-                          {
-                              label:'Print',
-                              icon:'pi pi-fw pi-print'
-                          }
-                      ]
-                  },
-                  {
-                      icon:'pi pi-fw pi-bars',
-                      label:'List'
-                  }
-                  ]
               }
           ]
       },
       {
           label:'Noticias',
           icon:'pi pi-fw pi-calendar',
-          items:[
-              {
-                  label:'Edit',
-                  icon:'pi pi-fw pi-pencil',
-                  items:[
-                  {
-                      label:'Save',
-                      icon:'pi pi-fw pi-calendar-plus'
-                  },
-                  {
-                      label:'Delete',
-                      icon:'pi pi-fw pi-calendar-minus'
-                  },
-
-                  ]
-              },
-              {
-                  label:'Archieve',
-                  icon:'pi pi-fw pi-calendar-times',
-                  items:[
-                  {
-                      label:'Remove',
-                      icon:'pi pi-fw pi-calendar-minus'
-                  }
-                  ]
-              }
-          ]
       },
   ];
   }
